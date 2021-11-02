7374b267ed099a18eadb3d7e78237d1c1b444c41
Antonio Moura <antonio.gmoura@gmail.com>
2021-11-01 20:43:50 -0400

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -11,13 +11,9 @@ public class Principal {
 		sc.close();
 	    System.out.println(saque);
 	    
-	    if (saque < 5.0) {
-	    	throw new Exception ("Saque Mínimo é R$ 5,00!");
+	    if (saque <= 0.0) {
+	    	throw new Exception ("Valor do saque inválido (zero ou negativo)!");
 	    }
-	    
-//	    if (saque % 5 > 0) {
-//	    	throw new Exception ("Saque deve ser múltiplo de R$ 5,00!");
-//	    }
 
 	    Double[] cedulas = {100.0, 50.0, 20.0, 10.0, 5.0};
 	    
